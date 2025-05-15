582a3cc549667fa200807dc08c237f0b62666c4d
Kawann <kawannmarcelo7@gmail.com>
2025-05-15 16:13:25 -0300

    feat: creating get line by code function

--- src/controllers/linha.js
+++ src/controllers/linha.js
@@ -8,3 +8,20 @@ export const getLines = async (req, res) => {
         res.status(500).json({ message: "Internal server error" });
     }
 }
+
+export const getLineByCode = async (req, res) => {
+    const { code } = req.params;
+    try {
+        const line = await prisma.linha.findFirst({
+            where: {
+                code: code
+            }
+        })
+        if (!line) {
+            return res.status(404).json({ message: "Line not found" });
+        }
+        res.status(200).json(line)
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
