0a605f5376298ae9918b254b1efbf9af42c1ffcd
Kawann <kawannmarcelo7@gmail.com>
2025-05-15 16:21:27 -0300

    fix: fix line import

--- src/routes/linha.js
+++ src/routes/linha.js
@@ -1,4 +1,4 @@
-import { getLines, getLineByCode } from "../controllers/linha";
+import { getLines, getLineByCode } from "../controllers/linha.js";
 import { Router } from "express";
 
 const router = Router();
