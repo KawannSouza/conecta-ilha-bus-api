c780924e931cdf626e49f7eef970ec07265d692e
Kawann <kawannmarcelo7@gmail.com>
2025-05-21 22:46:36 -0300

    porta do deploy

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import user from './routes/user.js';
 import linha from './routes/linha.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
